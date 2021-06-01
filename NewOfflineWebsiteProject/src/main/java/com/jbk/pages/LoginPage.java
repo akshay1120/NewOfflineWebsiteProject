aaee443295b134f218c28e79c691169da6fcfe34
akshay1120 <asjain3031@gmail.com>
2021-06-01 20:31:30 +0530

    new Sprint-1

--- NewOfflineWebsiteProject/src/main/java/com/jbk/pages/LoginPage.java
+++ NewOfflineWebsiteProject/src/main/java/com/jbk/pages/LoginPage.java
@@ -1,8 +1,13 @@
 package com.jbk.pages;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import com.jbk.objectRepository.DashboardPgObjRepo;
 import com.jbk.objectRepository.LoginPgObjRepo;
 import com.utility.Utility;
 
@@ -15,6 +20,8 @@ public class LoginPage extends LoginPgObjRepo
 		this.driver = driver ;
 		PageFactory.initElements(driver, this);
 	}
+	
+	DashboardPgObjRepo dpor = new DashboardPgObjRepo();
 	
 	//1
 	public boolean url()
@@ -92,6 +99,275 @@ public class LoginPage extends LoginPgObjRepo
 		{
 			log.info("Actual and Expected Headings are matched");
 			return false ;
+		}
+	}
+	
+//5
+	public boolean subHeading()
+	{
+		String actualSubHeading = Utility.getText(subHeading);
+		log.info("Actual Subheading = " + actualSubHeading);
+		
+		String expectedSubHeading = "JAVA | SELENIUM | PYTHON" ;
+		log.info("Expected Subheading = " + expectedSubHeading);
+		
+		if (actualSubHeading.equals(expectedSubHeading))
+		{
+			log.info("Actual and expected Subheadings are matched");
+			return true ;
+		}
+		else
+		{
+			log.info("Actual and expected Subheadings are not matched");
+			return false ;
+		}
+	}
+	
+	//6
+	public boolean signInBoxHeading()
+	{
+		String actualSignInBoxHeading = Utility.getText(signInBoxHeading);
+		log.info("Actual Sign-in box heading = " + actualSignInBoxHeading);
+		
+		String expectedSignInBoxHeading = "Sign in to start your session" ;
+		log.info("Expected Sign-in box heading = " + expectedSignInBoxHeading);
+		
+		if (actualSignInBoxHeading.equals(expectedSignInBoxHeading))
+		{
+			log.info("Actual and Expected Sign-in box headings are matched");
+			return true ;
+		}
+		else
+		{
+			log.info("Actual and Expected Sign-in box headings are not matched");
+			return false ;
+		}
+	}
+	
+	//7
+	public boolean placeholders()
+	{
+		ArrayList <String>expPlaceholderText = new ArrayList <String>();
+		expPlaceholderText.add("Email");
+		expPlaceholderText.add("Password");
+		log.info("List of Expected Placeholders = " + expPlaceholderText);
+		
+		ArrayList <String>actPlaceholderText = Utility.getPlaceholdersList(placeholdersList);
+		log.info("List of Actual Placeholders = " + actPlaceholderText);
+		
+		if (actPlaceholderText.equals(expPlaceholderText))
+		{
+			log.info("List of Actual and Expected Placeholders are matched");
+			return true ;
+		}
+		else
+		{
+			log.info("List of Actual and Expected Placeholders are not matched");
+			return false ;
+		}
+	}
+	
+	//8
+	public boolean validLogIn() 
+	{
+		Utility.sendkeys(uname, prop.getProperty("uname"));
+		Utility.sendkeys(pass, prop.getProperty("password"));
+		Utility.click(lgnBtn);
+		
+		String expectedTitle = "JavaByKiran | Dashboard" ; 
+		log.info("Expected Title = " + expectedTitle);
+		
+		String actualTitle = Utility.getTitle(driver);
+		log.info("Actual Title = " + actualTitle);
+		
+		if (actualTitle.equals(expectedTitle))
+		{
+			log.info("Actual and Expected titles are matched");
+			return true ;
+		}
+		else
+		{
+			log.info("Actual and Expected titles are not matched");
+			return false ;
+		}
+	}
+	
+	//9
+	public boolean logoutSuccessfullyMessage()
+	{
+		Utility.sendkeys(uname, prop.getProperty("uname"));
+		Utility.sendkeys(pass, prop.getProperty("password"));
+		Utility.click(lgnBtn);
+		
+		String expectedTitle = "JavaByKiran | Dashboard" ; 
+		log.info("Expected Title = " + expectedTitle);
+		
+		String actualTitle = Utility.getTitle(driver);
+		log.info("Actual Title = " + actualTitle);
+		
+		String actMessage = Utility.getText(successfullyLgOutMsg);
+		log.info("Actual Message = " + actMessage);
+		
+		String expMessage = "Logout successfully" ;
+		log.info("Expected Message = " + expMessage);
+		
+		if (actMessage.equals(expMessage))
+		{
+			log.info("Actual and Expected Message matched" );	
+			return true ;
+		}
+		else
+		{
+			log.info("Actual and Expected Message not matched" );
+			return false ;
+		}
+	}
+	
+	//10
+	public boolean invalidLogIn() 
+	{
+		Utility.sendkeys(uname, "akshay@gmail.com");
+		Utility.sendkeys(pass, "123456789");
+		Utility.click(lgnBtn);
+		
+		String expectedTitle = "JavaByKiran | Log in" ; 
+		log.info("Expected Title = " + expectedTitle);
+		
+		String actualTitle = Utility.getTitle(driver);
+		log.info("Actual Title = " + actualTitle);
+		
+		if (actualTitle.equals(expectedTitle))
+		{
+			log.info("Actual and Expected titles are matched");
+			return true ;
+		}
+		else
+		{
+			log.info("Actual and Expected titles are not matched");
+			return false ;
+		}
+	}
+	
+	//11
+	public boolean invalidLogInErrorMessage()
+	{
+		Utility.sendkeys(uname, "akshay@gmail.com");
+		Utility.sendkeys(pass, "123456789");
+		Utility.click(lgnBtn);
+		
+		ArrayList <String>expErrorMessage = new ArrayList <String>();
+		expErrorMessage.add("Please enter email as kiran@gmail.com");
+		expErrorMessage.add("Please enter password 123456");
+		log.info("List of Expected Error Messages = " + expErrorMessage);
+		
+		ArrayList <String>actErrorMessage = Utility.getListOfElements(ErrorsList);
+		log.info("List of Actual Error Messages = " + actErrorMessage);
+		
+		if (actErrorMessage.equals(expErrorMessage))
+		{
+			log.info("List of Expected and Actual Error Messages matched ");
+			return true ;
+		}
+		else
+		{
+			log.info("List of Expected and Actual Error Messages not matched ");
+			return false ;
+		}
+	}
+	
+	//12
+	public boolean blankLogInErrorMessage() 
+	{
+		Utility.clear(uname);
+		Utility.clear(pass);
+		Utility.click(lgnBtn);
+		
+		ArrayList <String>expErrorMessage = new ArrayList <String>();
+		expErrorMessage.add("Please enter email.");
+		expErrorMessage.add("Please enter password.");
+		log.info("List of Expected Error Messages = " + expErrorMessage);
+		
+		ArrayList <String>actErrorMessage = Utility.getListOfElements(ErrorsList);
+		log.info("List of Actual Error Messages = " + actErrorMessage);
+		
+		if (actErrorMessage.equals(expErrorMessage))
+		{
+			log.info("List of Expected and Actual Error Messages matched ");
+			return true ;
+		}
+		else
+		{
+			log.info("List of Expected and Actual Error Messages not matched ");
+			return false ;
+		}
+	}
+	
+	//13
+	public boolean registerLinkText() 
+	{ 
+		String actualRegisterLinkText = Utility.getText(registerLink);
+		log.info("Actual Register LinkText = " + actualRegisterLinkText);
+		
+		String expectedRegisterLinkText = "Register a new membership" ;
+		log.info("expected Register LinkText = " + expectedRegisterLinkText);
+		
+		if (actualRegisterLinkText.equals(expectedRegisterLinkText))
+		{
+			log.info("Actual and Expected Register LinkText matched");
+			return true ;
+		}
+		else
+		{
+			log.info("Actual and Expected Register LinkText not matched");
+			return false ;
+		}
+	}
+	
+	//14
+	public boolean registerLink() 
+	{ 
+		Utility.click(registerLink);
+		log.info("Clicked the Register Link");
+	
+		String expectedTitle = "JavaByKiran | Registration Page" ; 
+		log.info("Expected Title = " + expectedTitle);
+		
+		String actualTitle = Utility.getTitle(driver);
+		log.info("Actual Title = " + actualTitle);
+		
+		if (actualTitle.equals(expectedTitle))
+		{
+			log.info("Actual and Expected titles are matched");
+			return true ;
+		}
+		else
+		{
+			log.info("Actual and Expected titles are not matched");
+			return false ;
+		}
+	}
+	
+	//15
+	public void loginWithAllData(HashMap<String,String> hm)
+	{
+		Set<String> keys = hm.keySet();
+		for(String key :keys)
+		{
+			Utility.sendkeys(uname, key);
+			Utility.sendkeys(pass,hm.get(key));
+			Utility.click(lgnBtn);
+			
+			if(Utility.getTitle(driver).contains("Dashboard"))
+			{
+				log.info("Valid Data = Username : "+ key + "    Password :" + hm.get(key));
+				Utility.click(dpor.leftLgOut);
+			}
+			else
+			{
+				log.info("invalid Data = Username : "+ key + "    Password :" + hm.get(key));
+				Utility.clear(uname);
+				Utility.clear(pass);
+			}
 		}
 	}
 	
--- NewOfflineWebsiteProject/src/test/java/com/jbk/tests/LoginTest.java
+++ NewOfflineWebsiteProject/src/test/java/com/jbk/tests/LoginTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 import com.jbk.pages.LoginPage;
 import com.jbk.testBase.TestBase;
+import com.utility.ExcelUtility;
 
 public class LoginTest extends TestBase
 {
@@ -51,5 +52,82 @@ public class LoginTest extends TestBase
 	{
 		log.info("verifying the MainHeading");
 		Assert.assertTrue(lp.mainHeading());
+	}
+	
+	@Test (priority=5)
+	public void verifySubHeading()
+	{
+		log.info("verifying the subHeading");
+		Assert.assertTrue(lp.subHeading());
+	}
+	
+	@Test (priority=6)
+	public void verifySignInBoxHeading()
+	{
+		log.info("verifying the signInBoxHeading");
+		Assert.assertTrue(lp.signInBoxHeading());
+	}
+	
+	@Test (priority=7)
+	public void verifyPlaceholders()
+	{
+		log.info("verifying the placeholders");
+		Assert.assertTrue(lp.placeholders());
+	}
+	
+	@Test (priority=8)
+	public void verifyValidLogIn()
+	{
+		log.info("verifying the valid LogIn");
+		Assert.assertTrue(lp.validLogIn());
+	}
+	
+	@Test (priority=9)
+	public void verifyLogoutSuccessfullyMessage()
+	{
+		log.info("verifying the logout Successfully Message");
+		Assert.assertTrue(lp.logoutSuccessfullyMessage());
+	}
+	
+	@Test (priority=10)
+	public void verifyInvalidLogIn()
+	{
+		log.info("verifying the invalid LogIn");
+		Assert.assertTrue(lp.invalidLogIn());
+	}
+	
+	@Test (priority=11)
+	public void verifyInvalidLogInErrorMessage()
+	{
+		log.info("verifying the invalid Log In Error Message");
+		Assert.assertTrue(lp.invalidLogInErrorMessage());
+	}
+	
+	@Test (priority=12)
+	public void verifyBlankLogInErrorMessage()
+	{
+		log.info("verifying the blank Log In Error Message");
+		Assert.assertTrue(lp.blankLogInErrorMessage());
+	}
+	
+	@Test (priority=13)
+	public void verifyRegisterLinkText()
+	{
+		log.info("verifying the register Link Text");
+		Assert.assertTrue(lp.registerLinkText());
+	}
+	
+	@Test (priority=14)
+	public void verifyRegisterLink()
+	{
+		log.info("verifying the register Link");
+		Assert.assertTrue(lp.registerLink());
+	}
+	
+	@Test (priority=15)
+	public void loginWithAlldata()throws Exception
+	{
+		log.info("verifying the Login Without Heading Excel Data");
+		lp.loginWithAllData(ExcelUtility.readUnameAndPass("Data.xlsx", "LoginWithoutHeading", 0, 1 , 0));
 	}
 }
